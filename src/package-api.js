c2141b7f7722b154a654a1561dc5ae15f338e411
Nikita Sergeev <sergeev.nickitos@yandex.ru>
2019-11-28 19:40:20 +0300

    common: added number, object, array, async utils

--- src/package-api.js
+++ src/package-api.js
@@ -1,4 +1,8 @@
 export { CommonUtils } from './utils/common/common.utils';
 export { AsyncUtils } from './utils/async/async.utils';
+export { StringUtils } from './utils/string/string.utils';
+export { ObjectUtils } from './utils/object/object.utils';
+export { NumberUtils } from './utils/number/number.utils';
+export { ArrayUtils } from './utils/array/array.utils';
 export { Logger } from './utils/logger/logger';
 export { LoggingLevelType } from './utils/logger/logger.enum';
