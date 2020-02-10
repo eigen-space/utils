7e89bd3de0dc4b1a8516f135e60e53690f0b2785
Nikita Sergeev <sergeev.nickitos@yandex.ru>
2020-02-10 16:00:33 +0700

    CF-1599 common: removed duplicated util

--- src/utils/common/common.utils.spec.ts
+++ src/utils/common/common.utils.spec.ts
@@ -56,44 +56,6 @@ describe('CommonUtils', () => {
         });
     });
 
-    describe('#debounce', () => {
-        const debounceTime = 100;
-
-        it('should call function after bounce time is over', done => {
-            const request = jest.fn();
-            const debouncedFunc: (arg: string) => void = CommonUtils.debounce(request, debounceTime);
-            debouncedFunc('1');
-            debouncedFunc('2');
-
-            const timeoutTime = 100;
-            setTimeout(() => {
-                expect(request).toHaveBeenCalledTimes(1);
-                expect(request).toHaveBeenCalledWith('2');
-                done();
-            }, timeoutTime);
-
-            expect(request).not.toBeCalled();
-        });
-
-        it('should call function after bounce time is over after call', done => {
-            expect.hasAssertions();
-            const request = jest.fn();
-            const debouncedFunc: (arg: string) => void = CommonUtils.debounce(request, debounceTime);
-            debouncedFunc('1');
-            debouncedFunc('2');
-
-            const timeoutTime = 200;
-            setTimeout(() => {
-                debouncedFunc('3');
-                debouncedFunc('4');
-
-                expect(request).toHaveBeenCalledTimes(1);
-                expect(request).toHaveBeenCalledWith('2');
-                done();
-            }, timeoutTime);
-        });
-    });
-
     describe('#getNotInvokable', () => {
         let props: AnyDictionary;
 
