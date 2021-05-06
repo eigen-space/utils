9636f05702a683c95ab10c2e2c6d98fd9468ffaf
Nikita Agupov <nktrsk@gmail.com>
2021-05-06 16:12:54 +0300

    microfix common: removed url utils

--- src/package-api.js
+++ src/package-api.js
@@ -4,5 +4,4 @@ export { StringUtils } from './utils/string/string.utils';
 export { ObjectUtils } from './utils/object/object.utils';
 export { NumberUtils } from './utils/number/number.utils';
 export { ArrayUtils } from './utils/array/array.utils';
-export { UrlUtils } from './utils/url/url.utils';
 export { DateUtils } from './utils/date/date.utils';
--- src/utils/url/url.utils.ts
+++
@@ -1,9 +0,0 @@
-import { URL } from 'url';
-import { Url } from '@eigenspace/common-types';
-
-export class UrlUtils {
-
-    static parse(url: Url): URL {
-        return new URL(url);
-    }
-}
